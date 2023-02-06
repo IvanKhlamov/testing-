f42e5b0b81d87d4a34babb5b0e868a1f973f09ad
IvanGoncharov <ivan.goncharov19.09@gmail.com>
2023-02-06 12:33:17 +0300

    Initial commit

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+a = float(input())
+b = float(input())
+c = float(input())
+discriminant = b**2 - 4*a*c
+if discriminant < 0:
+    print('Корней нет')
+elif discriminant == 0:
+    x = -b / (2 * a)
+    print('x = ' + str(x))
+else:
+    x1 = (-b + discriminant ** 0.5) / (2 * a)
+    x2 = (-b - discriminant ** 0.5) / (2 * a)
+    print('x₁ = ' + str(x1))
+    print('x₂ = ' + str(x2))
